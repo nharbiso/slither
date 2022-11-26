3b5559be0f633aacd6b3d4a7a54c307f198c1292
karankashyap04 <karankashyap@gmail.com>
2022-11-25 23:09:42 -0500

    fix: Add string analogs for client-side MessageType enum values to enable server-side deserialization

--- client/src/message/messageTypes.ts
+++ client/src/message/messageTypes.ts
@@ -1,10 +1,10 @@
 enum MessageType {
-  NEW_CLIENT,
-  CLIENT_LEFT,
-  GET_LEADERBOARD,
-  UPDATE_SCORE,
-  ERROR,
-  SUCCESS,
+  NEW_CLIENT = "NEW_CLIENT",
+  CLIENT_LEFT = "CLIENT_LEFT",
+  GET_LEADERBOARD = "GET_LEADERBOARD",
+  UPDATE_SCORE = "UPDATE_SCORE",
+  ERROR = "ERROR",
+  SUCCESS = "SUCCESS",
   // Update this as more code is developed
 }
 
