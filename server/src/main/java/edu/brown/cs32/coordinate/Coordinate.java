d411e0bef48d5051b60717cff3c7458155dba58f
karankashyap04 <karankashyap@gmail.com>
2022-11-29 02:17:55 -0500

    feat: Add classes for Coordinates, Orbs, OrbSizes

---
+++ server/src/main/java/edu/brown/cs32/coordinate/Coordinate.java
@@ -0,0 +1,3 @@
+package edu.brown.cs32.coordinate;
+
+public record Coordinate(float x, float y) {}
---
+++ server/src/main/java/edu/brown/cs32/orb/Orb.java
@@ -0,0 +1,5 @@
+package edu.brown.cs32.orb;
+
+import edu.brown.cs32.coordinate.Coordinate;
+
+public record Orb(Coordinate coordinate, OrbSize orbSize) {}
