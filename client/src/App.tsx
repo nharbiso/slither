aa5decb010a8443c6cc5cbf2f0044d091902726e
karankashyap04 <karankashyap@gmail.com>
2022-12-02 00:03:55 -0500

    feat: Add client-side leaderboard update code and move socket registration to Game component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,58 +11,59 @@ import OrbSize from "./orb/orbSize";
 import Orb, { OrbInfo } from "./orb/Orb";
 import MessageType from "./message/messageTypes";
 
-const AppConfig = {
-  PROTOCOL: "ws:",
-  HOST: "//localhost",
-  PORT: ":9000",
-};
+// const AppConfig = {
+//   PROTOCOL: "ws:",
+//   HOST: "//localhost",
+//   PORT: ":9000",
+// };
 
-let toregister: boolean = true;
-let socket: WebSocket;
+// let toregister: boolean = true;
+// let socket: WebSocket;
 
-function registerSocket() {
-  let socket: WebSocket = new WebSocket(
-    AppConfig.PROTOCOL + AppConfig.HOST + AppConfig.PORT
-  );
+// function registerSocket() {
+//   let socket: WebSocket = new WebSocket(
+//     AppConfig.PROTOCOL + AppConfig.HOST + AppConfig.PORT
+//   );
 
-  socket.onopen = () => {
-    console.log("client: A new client-side socket was opened!");
-    // TODO: random string username for now; pass user chosen username later); also pass chosen game code later
-    sendNewClientNoCodeMessage(socket, (Math.random() * 1000).toString());
-    // sendNewClientWithCodeMessage(
-    //   socket,
-    //   (Math.random() * 1000).toString(),
-    //   "123456"
-    // );
-  };
+//   socket.onopen = () => {
+//     console.log("client: A new client-side socket was opened!");
+//     // TODO: random string username for now; pass user chosen username later); also pass chosen game code later
+//     sendNewClientNoCodeMessage(socket, (Math.random() * 1000).toString());
+//     // sendNewClientWithCodeMessage(
+//     //   socket,
+//     //   (Math.random() * 1000).toString(),
+//     //   "123456"
+//     // );
+//   };
 
-  // socket.onmessage = (response: MessageEvent) => {
-  //   let message = JSON.parse(response.data);
-  //   // ideally, we would want to do different things based on the message's type
-  //   console.log("client: A message was received: " + response.data);
-  //   switch (message.type) {
-  //     case MessageType.UPDATE_LEADERBOARD: {
-  //       const leaderboardMessage: leaderboardData = message;
-  //     }
-  //   }
-  // };
-}
+//   socket.onmessage = (response: MessageEvent) => {
+//     let message = JSON.parse(response.data);
+//     // ideally, we would want to do different things based on the message's type
+//     console.log("client: A message was received: " + response.data);
+//     switch (message.type) {
+//       case MessageType.UPDATE_LEADERBOARD: {
+//         const leaderboardMessage: leaderboardData = message;
+//       }
+//     }
+//   };
+// }
 
 const orbInfo: OrbInfo = { x: 100, y: 500, size: OrbSize.LARGE };
 
 function App() {
-  useEffect(() => {
-    if (!toregister) {
-      return;
-    }
-    toregister = false;
-    registerSocket();
-  }, []);
+  // useEffect(() => {
+  //   if (!toregister) {
+  //     return;
+  //   }
+  //   toregister = false;
+  //   registerSocket();
+  // }, []);
   // registerSocket();
 
   return (
     <div className="App">
-      <Game socket={socket} />
+      {/* <Game socket={socket} /> */}
+      <Game />
       <Orb orbInfo={orbInfo} />
     </div>
   );
--- client/src/game/Game.tsx
+++ client/src/game/Game.tsx
@@ -1,56 +1,103 @@
-import Denque from "denque"
-import React, { useState } from "react"
-import { SnakeData, Position, SNAKE_VELOCITY} from "./Snake"
+import Denque from "denque";
+import React, { useState, useEffect } from "react";
+import { SnakeData, Position, SNAKE_VELOCITY } from "./Snake";
 
-import GameCanvas from "./GameCanvas"
+import GameCanvas from "./GameCanvas";
 import MessageType from "../message/messageTypes";
-import { leaderboardData, leaderboardEntry } from "../message/message";
+import {
+  leaderboardData,
+  leaderboardEntry,
+  sendNewClientNoCodeMessage,
+} from "../message/message";
 import Leaderboard from "../leaderboard/Leaderboard";
 
-export default function Game({socket}: {socket: WebSocket}) {
-    const snakeBody: Position[] = [];
-    for(let i = 0; i < 100; i++) {
-        snakeBody.push({x: 600, y: 100 + 5 * i});
-    }
-    const snake: SnakeData = {
-        snakeBody: new Denque(snakeBody),
-        velocityX: 0,
-        velocityY: SNAKE_VELOCITY,
-    }
+const AppConfig = {
+  PROTOCOL: "ws:",
+  HOST: "//localhost",
+  PORT: ":9000",
+};
 
-    const [scores, setScores] = useState(new Map<string, number>());
+let toregister: boolean = true;
+let socket: WebSocket;
 
-    socket.onmessage = (response: MessageEvent) => {
-        let message = JSON.parse(response.data);
-        // ideally, we would want to do different things based on the message's type
-        console.log("client: A message was received: " + response.data);
+function registerSocket(
+  setScores: React.Dispatch<React.SetStateAction<Map<string, number>>>
+) {
+  let socket: WebSocket = new WebSocket(
+    AppConfig.PROTOCOL + AppConfig.HOST + AppConfig.PORT
+  );
+
+  socket.onopen = () => {
+    console.log("client: A new client-side socket was opened!");
+    // TODO: random string username for now; pass user chosen username later); also pass chosen game code later
+    sendNewClientNoCodeMessage(socket, (Math.random() * 1000).toString());
+    // sendNewClientWithCodeMessage(
+    //   socket,
+    //   (Math.random() * 1000).toString(),
+    //   "123456"
+    // );
+  };
+
+  socket.onmessage = (response: MessageEvent) => {
+    let message = JSON.parse(response.data);
+    // ideally, we would want to do different things based on the message's type
+    console.log("client: A message was received: " + response.data);
+    switch (message.type) {
+      case MessageType.UPDATE_LEADERBOARD: {
+        const leaderboardMessage: leaderboardData = message;
         switch (message.type) {
           case MessageType.UPDATE_LEADERBOARD: {
             const leaderboardMessage: leaderboardData = message;
-            setScores(extractLeaderboardMap(leaderboardMessage.data.leaderboard));
+            setScores(
+              extractLeaderboardMap(leaderboardMessage.data.leaderboard)
+            );
           }
         }
-      };
+      }
+    }
+  };
+}
 
-    const [snakes, setSnakes] = useState<SnakeData[]>([snake]);
-    return (
-        <div>
-            <GameCanvas snakes={snakes} setSnakes={setSnakes} mySnake={0}/>
-            <Leaderboard leaderboard={scores} />
-        </div>
-        //player's score
-    )
+export default function Game() {
+  const snakeBody: Position[] = [];
+  for (let i = 0; i < 100; i++) {
+    snakeBody.push({ x: 600, y: 100 + 5 * i });
+  }
+  const snake: SnakeData = {
+    snakeBody: new Denque(snakeBody),
+    velocityX: 0,
+    velocityY: SNAKE_VELOCITY,
+  };
+
+  const [scores, setScores] = useState(new Map<string, number>());
+
+  useEffect(() => {
+    if (!toregister) {
+      return;
+    }
+    toregister = false;
+    registerSocket(setScores);
+  }, []);
+
+  const [snakes, setSnakes] = useState<SnakeData[]>([snake]);
+  return (
+    <div>
+      <GameCanvas snakes={snakes} setSnakes={setSnakes} mySnake={0} />
+      <Leaderboard leaderboard={scores} />
+    </div>
+    //player's score
+  );
 }
 
 function extractLeaderboardMap(leaderboardData: leaderboardEntry[]) {
-    const leaderboard: Map<string, number> = new Map<string, number>();
-    leaderboardData.forEach((entry: leaderboardEntry) => {
-        leaderboard.set(entry.username, entry.score);
-    })
-    return leaderboard;
+  const leaderboard: Map<string, number> = new Map<string, number>();
+  leaderboardData.forEach((entry: leaderboardEntry) => {
+    leaderboard.set(entry.username, entry.score);
+  });
+  return leaderboard;
 }
 
-//here we want to take in gamestate data and load everything 
+//here we want to take in gamestate data and load everything
 //i.e. load canvas, put snakes on top, render orbs, check mouse movements to move the snake, send data back to server, etc.
 //essentially we should be calling everything from here
 
