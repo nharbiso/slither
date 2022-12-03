47c0729d6dde81c3ba30a68a2b473428c3dbed78
mason-pan <masonpan0244@gmail.com>
2022-12-02 21:00:47 -0500

    feat: added notes about collision, preliminary testing using getBoundingRectClient() did not work

--- client/src/game/GameCanvas.tsx
+++ client/src/game/GameCanvas.tsx
@@ -70,8 +70,11 @@ function moveSnake(snake: SnakeData, gameState: GameState, socket: WebSocket): S
         snake.snakeBody.unshift({x: newPosition.x, y: newPosition.y});
 
         if (gameState.otherBodies.has(newPosition)) { //might not be as straightforward as just checking if the position is occupied, need to check if the displayed circles are touching
+            //might want to use element.getBoundingClientRect() to check the actual edges of the div but that might not be accurate since we are using circles
             sendUserDiedMessage(socket)
-            console.log("u died")
+            console.log("u died") //this doesn't work right now
+            //also need to figure out what kind of return type we want in the case that we do run into another snake, moveSnake returns SnakeData which might not
+            //be suitable for telling GameCanvas that this snake is dead (problem is updating on client side, sendUserDiedMessage will tell the server)
             //need to make snake disappear and show some kind of message indicating u died
         }
         // if (allOrbs.has(newPosition)) { //need to somehow get the set of allOrbs (set of Positions representing every orb)
