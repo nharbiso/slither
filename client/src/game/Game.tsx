ec42115570c1febb05e70bd7e7692d413af3126b
plestz <paul_lestz@brown.edu>
2022-12-10 16:26:54 -0500

    style: removed outdated comments

--- client/src/game/Game.tsx
+++ client/src/game/Game.tsx
@@ -112,52 +112,13 @@ export function registerSocket(
         break;
       }
       case MessageType.SEND_ORBS: {
-        // const orbPosition: Position = {
-        //   x: 100,
-        //   y: 500,
-        // };
-        // const orb: OrbData = { position: orbPosition, size: OrbSize.LARGE };
         orbSet = message.data.orbSet;
-        // setOrbSet(message.data.orbSet);
-        // let gs: GameState = gameState;
         gameState.orbs = orbSet;
         setGameState(gameState);
 
-        console.log("orbSet");
-        console.log(orbSet);
-
-        console.log("test");
-        console.log(Array.from(orbSet));
-        console.log("0");
-        console.log(Array.from(orbSet)[0]);
-        console.log(Array.from(orbSet)[0].orbSize);
-        // let p: Position = {x: 10, y: 10};
-        // let o: OrbData = {position: p, size: OrbSize.LARGE};
-        // console.log(typeof(o));
-
-        // console.log('reached');
-        // console.log(typeof(message.data.orbSet[0]));
-        // let orb: OrbData = message.data.orbSet[0];
-        // console.log('orb: ');
-        // console.log(orb);
-        // console.log(typeof(orb));
-
-        // let orbs: Set<OrbData> = message.data.orbSet;
-        // console.log('orbs: ');
-        // console.log(orbs);
-        // console.log(typeof(orbs));
-
-        // console.log(message.data.orbSet);
-        // setOrbSet(message.data.orbSet);
-        // console.log('orbset!!!');
-        // console.log(orbSet);
         break;
       }
     }
-
-    // case MessageType.SET_CODE: {
-
-    // }
   };
 }
 
@@ -178,44 +139,6 @@ export default function Game({
   gameCode,
   setGameCode,
 }: GameProps) {
-  // const snakeBody: Position[] = [];
-  // for (let i = 0; i < 100; i++) {
-  //   snakeBody.push({ x: 600, y: 100 + 5 * i });
-  // }
-  // const snake: SnakeData = {
-  //   snakeBody: new Denque(snakeBody),
-  //   velocityX: 0,
-  //   velocityY: SNAKE_VELOCITY,
-  // };
-
-  // const [scores, setScores] = useState(new Map<string, number>());
-
-  // useEffect(() => {
-  //   if (!toregister) {
-  //     return;
-  //   }
-  //   toregister = false;
-  //   registerSocket(setScores);
-  // }, []);
-
-  const [snakes, setSnakes] = useState<SnakeData[]>([
-    gameState.snakes.get("user1")!,
-  ]);
-
-  // const position: Position = {
-  //   x: 100,
-  //   y: 500,
-  // };
-
-  // const orb: OrbData = { position, size: OrbSize.LARGE };
-
-  // const [gameState, setGameState] = useState<GameState>({
-  //   snakes: new Map([["user1", snake]]),
-  //   otherBodies: new Set(),
-  //   orbs: new Set([orb]),
-  //   scores: new Map([["user1", 0]]),
-  //   gameCode: "abc",
-  // });
 
   return (
     <div>
